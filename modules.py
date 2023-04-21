228951795d56243949ea0b482b27b2523d27c796
doorleyr <doorleyr@tcd.ie>
2023-04-21 20:43:46 +0000

    Refactored and made the model conditional on continuous inputs, not labels.

--- modules.py
+++ modules.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -126,28 +125,28 @@ class Up(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self, c_in=3, c_out=3, time_dim=256, device="cuda"):
+    def __init__(self, image_size, c_in=3, c_out=3, time_dim=256, device="cuda"):
         super().__init__()
         self.device = device
         self.time_dim = time_dim
         self.inc = DoubleConv(c_in, 64)
         self.down1 = Down(64, 128)
-        self.sa1 = SelfAttention(128, 32)
+        self.sa1 = SelfAttention(128, int(image_size/2))
         self.down2 = Down(128, 256)
-        self.sa2 = SelfAttention(256, 16)
+        self.sa2 = SelfAttention(256, int(image_size/4))
         self.down3 = Down(256, 256)
-        self.sa3 = SelfAttention(256, 8)
+        self.sa3 = SelfAttention(256, int(image_size/8))
 
         self.bot1 = DoubleConv(256, 512)
         self.bot2 = DoubleConv(512, 512)
         self.bot3 = DoubleConv(512, 256)
 
         self.up1 = Up(512, 128)
-        self.sa4 = SelfAttention(128, 16)
+        self.sa4 = SelfAttention(128, int(image_size/4))
         self.up2 = Up(256, 64)
-        self.sa5 = SelfAttention(64, 32)
+        self.sa5 = SelfAttention(64, int(image_size/2))
         self.up3 = Up(128, 64)
-        self.sa6 = SelfAttention(64, 64)
+        self.sa6 = SelfAttention(64, int(image_size))
         self.outc = nn.Conv2d(64, c_out, kernel_size=1)
 
     def pos_encoding(self, t, channels):
@@ -166,53 +165,54 @@ class UNet(nn.Module):
 
         x1 = self.inc(x)
         x2 = self.down1(x1, t)
-        x2 = self.sa1(x2)
+        # x2 = self.sa1(x2)
         x3 = self.down2(x2, t)
-        x3 = self.sa2(x3)
+        # x3 = self.sa2(x3)
         x4 = self.down3(x3, t)
         x4 = self.sa3(x4)
 
         x4 = self.bot1(x4)
-        x4 = self.bot2(x4)
+        # x4 = self.bot2(x4)
         x4 = self.bot3(x4)
 
         x = self.up1(x4, x3, t)
         x = self.sa4(x)
         x = self.up2(x, x2, t)
-        x = self.sa5(x)
+        # x = self.sa5(x)
         x = self.up3(x, x1, t)
-        x = self.sa6(x)
+        # x = self.sa6(x)
         output = self.outc(x)
         return output
 
 
 class UNet_conditional(nn.Module):
-    def __init__(self, c_in=3, c_out=3, time_dim=256, num_classes=None, device="cuda"):
+    def __init__(self, image_size, c_in=3, c_out=3, time_dim=256, num_classes=None, device="cuda"):
         super().__init__()
         self.device = device
         self.time_dim = time_dim
         self.inc = DoubleConv(c_in, 64)
         self.down1 = Down(64, 128)
-        self.sa1 = SelfAttention(128, 32)
+        self.sa1 = SelfAttention(128, int(image_size/2))
         self.down2 = Down(128, 256)
-        self.sa2 = SelfAttention(256, 16)
+        self.sa2 = SelfAttention(256, int(image_size/4))
         self.down3 = Down(256, 256)
-        self.sa3 = SelfAttention(256, 8)
+        self.sa3 = SelfAttention(256, int(image_size/8))
 
         self.bot1 = DoubleConv(256, 512)
         self.bot2 = DoubleConv(512, 512)
         self.bot3 = DoubleConv(512, 256)
 
         self.up1 = Up(512, 128)
-        self.sa4 = SelfAttention(128, 16)
+        self.sa4 = SelfAttention(128, int(image_size/4))
         self.up2 = Up(256, 64)
-        self.sa5 = SelfAttention(64, 32)
+        self.sa5 = SelfAttention(64, int(image_size/2))
         self.up3 = Up(128, 64)
-        self.sa6 = SelfAttention(64, 64)
+        self.sa6 = SelfAttention(64, int(image_size))
         self.outc = nn.Conv2d(64, c_out, kernel_size=1)
 
         if num_classes is not None:
-            self.label_emb = nn.Embedding(num_classes, time_dim)
+            # self.label_emb = nn.Embedding(num_classes, time_dim)
+            self.label_emb = nn.Linear(num_classes, time_dim, bias=False)
 
     def pos_encoding(self, t, channels):
         inv_freq = 1.0 / (
@@ -233,22 +233,22 @@ class UNet_conditional(nn.Module):
 
         x1 = self.inc(x)
         x2 = self.down1(x1, t)
-        x2 = self.sa1(x2)
+        # x2 = self.sa1(x2)
         x3 = self.down2(x2, t)
-        x3 = self.sa2(x3)
+        # x3 = self.sa2(x3)
         x4 = self.down3(x3, t)
         x4 = self.sa3(x4)
 
         x4 = self.bot1(x4)
-        x4 = self.bot2(x4)
+        # x4 = self.bot2(x4)
         x4 = self.bot3(x4)
 
         x = self.up1(x4, x3, t)
-        x = self.sa4(x)
+        # x = self.sa4(x)
         x = self.up2(x, x2, t)
         x = self.sa5(x)
         x = self.up3(x, x1, t)
-        x = self.sa6(x)
+        # x = self.sa6(x)
         output = self.outc(x)
         return output
 
--- utils.py
+++ utils.py
@@ -6,12 +6,12 @@ from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
 
 
-def plot_images(images):
-    plt.figure(figsize=(32, 32))
-    plt.imshow(torch.cat([
-        torch.cat([i for i in images.cpu()], dim=-1),
-    ], dim=-2).permute(1, 2, 0).cpu())
-    plt.show()
+# def plot_images(images):
+#     plt.figure(figsize=(32, 32))
+#     plt.imshow(torch.cat([
+#         torch.cat([i for i in images.cpu()], dim=-1),
+#     ], dim=-2).permute(1, 2, 0).cpu())
+#     plt.show()
 
 
 def save_images(images, path, **kwargs):
@@ -21,16 +21,16 @@ def save_images(images, path, **kwargs):
     im.save(path)
 
 
-def get_data(args):
-    transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(80),  # args.image_size + 1/4 *args.image_size
-        torchvision.transforms.RandomResizedCrop(args.image_size, scale=(0.8, 1.0)),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    dataset = torchvision.datasets.ImageFolder(args.dataset_path, transform=transforms)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
-    return dataloader
+# def get_data(args):
+#     transforms = torchvision.transforms.Compose([
+#         torchvision.transforms.Resize(80),  # args.image_size + 1/4 *args.image_size
+#         torchvision.transforms.RandomResizedCrop(args.image_size, scale=(0.8, 1.0)),
+#         torchvision.transforms.ToTensor(),
+#         torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+#     ])
+#     dataset = torchvision.datasets.ImageFolder(args.dataset_path, transform=transforms)
+#     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+#     return dataloader
 
 
 def setup_logging(run_name):
